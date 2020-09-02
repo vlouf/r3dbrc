314646258208e7405b9c5f256f5d2e2f0371ba00
Valentin Louf <valentin.louf@bom.gov.au>
2020-09-02 11:03:25 +1000

    ADDED: args parser

--- pyr3dbrc.py
+++ pyr3dbrc.py
@@ -27,17 +27,17 @@ def get_radar_archive_file(date, rid):
     """
     if type(rid) is not str:
         rid = f"{rid:02}"
-        
+
     datestr = date.strftime("%Y%m%d")
     file = f"/g/data/rq0/admin/level_1b/grid_150km/{rid}/{date.year}/{rid}_{datestr}_level1b_grid_150km.zip"
     if not os.path.exists(file):
-        print(f"{file} does not exist.")
+        raise FileNotFoundError(f"{file} does not exist for radar {rid}.")
         return None
 
     return file
 
 
-def extract_zip(inzip, date, path="/scratch/kl02/vhl548/unzipdir"):
+def extract_zip(inzip, date):
     """
     Extract file in a daily archive zipfile for a specific datetime.
 
@@ -54,17 +54,18 @@ def extract_zip(inzip, date, path="/scratch/kl02/vhl548/unzipdir"):
     grfile: str
         Output ground radar file.
     """
+
     def get_zipfile_name(namelist, date):
         datestr = [re.findall("[0-9]{8}_[0-9]{6}", n)[0] for n in namelist]
         timestamps = np.array([datetime.datetime.strptime(dt, "%Y%m%d_%H%M%S") for dt in datestr], dtype="datetime64")
-        pos = np.argmin(np.abs(timestamps - date.to_numpy()))        
+        pos = np.argmin(np.abs(timestamps - date.to_numpy()))
         grfile = namelist[pos]
         return grfile
 
     with zipfile.ZipFile(inzip) as zid:
         namelist = zid.namelist()
         file = get_zipfile_name(namelist, date)
-        zid.extract(file, path=path)
+        zid.extract(file, path=UNZIPPATH)
 
     grfile = os.path.join(path, file)
 
@@ -80,15 +81,14 @@ def get_cpol_file(date):
 
     datelist = [re.findall("[0-9]{8}.[0-9]{6}", n)[0] for n in namelist]
     timestamps = np.array([datetime.datetime.strptime(dt, "%Y%m%d.%H%M%S") for dt in datelist], dtype="datetime64")
-    pos = np.argmin(np.abs(timestamps - date.to_numpy()))    
+    pos = np.argmin(np.abs(timestamps - date.to_numpy()))
     grfile = namelist[pos]
-    
+
     return grfile
 
 
 def main():
-    input_date = datetime.datetime(2014, 12, 1, 6, 30)
-    date = [pd.Timestamp(input_date)]
+    date = [INDATE]
     date.append(date[0] + pd.Timedelta("10Min"))
     files = [get_cpol_file(d) for d in date]
     zips = [get_radar_archive_file(d, 64) for d in date]
@@ -96,10 +96,35 @@ def main():
     for z, d in zip(zips, date):
         files.append(extract_zip(z, d))
 
-    with open("r3dbrc", "w+") as fid:
+    with open(os.path.join("r3dbrc"), "w+") as fid:
         fid.write("\n".join(files))
 
     return None
 
+
 if __name__ == "__main__":
+    parser_description = "Generate the r3dbrc file for 3D Winds."
+    parser = argparse.ArgumentParser(description=parser_description)
+    parser.add_argument("-r", "--rid", dest="rid", type=int, help="Radar Rapic ID", default=64)
+    parser.add_argument(
+        "-o", "--output", dest="outdir", type=str, help="Output directory.", default=os.curdir,
+    )
+    parser.add_argument(
+        "-u",
+        "--unzip",
+        dest="unzip",
+        type=str,
+        help="Unzipping temporary directory (files won't be deleted!).",
+        default=os.curdir,
+    )
+
+    parser.add_argument(
+        "-d", "--date", dest="date", type=str, help="Datetime format: 201703041210 or 2017-03-04T12:10", required=True
+    )
+    args = parser.parse_args()
+    INDATE = pd.Timestamp(args.date)
+    OUTPATH = args.outdir
+    UNZIPPATH = args.unzip
+    RID = args.rid
+
     main()
